7b67cd20387d45bb5990ae3438dff74f424c2cf6
Krivodel <rus34honor7x@gmail.com>
2026-07-18 22:31:39 +0300

    test: centralize motion easing samples

--- tests/AtomicArt.Desktop.Tests/Services/GalleryAnimation/MotionEasingTests.cs
+++ tests/AtomicArt.Desktop.Tests/Services/GalleryAnimation/MotionEasingTests.cs
@@ -7,47 +7,34 @@ namespace AtomicArt.Desktop.Tests.Services.GalleryAnimation;
 
 public sealed class MotionEasingTests
 {
+    public static TheoryData<Func<double, double>, double, double> ReferenceSamples =>
+        new TheoryData<Func<double, double>, double, double>
+        {
+            { MotionEasing.EaseOut, 0d, 0d },
+            { MotionEasing.EaseOut, 0.25d, 0.578125d },
+            { MotionEasing.EaseOut, 0.5d, 0.875d },
+            { MotionEasing.EaseOut, 0.75d, 0.984375d },
+            { MotionEasing.EaseOut, 1d, 1d },
+            { MotionEasing.EaseRail, 0d, 0d },
+            { MotionEasing.EaseRail, 0.25d, 0.44565392967742323d },
+            { MotionEasing.EaseRail, 0.5d, 0.81900414413722489d },
+            { MotionEasing.EaseRail, 0.75d, 0.96352085669164911d },
+            { MotionEasing.EaseRail, 1d, 1d },
+            { MotionEasing.EaseMaterial, 0d, 0d },
+            { MotionEasing.EaseMaterial, 0.25d, 0.812131184615433d },
+            { MotionEasing.EaseMaterial, 0.5d, 0.95683848994775467d },
+            { MotionEasing.EaseMaterial, 0.75d, 0.99303416534723565d },
+            { MotionEasing.EaseMaterial, 1d, 1d }
+        };
+
     [Theory]
-    [InlineData(0d, 0d)]
-    [InlineData(0.25d, 0.578125d)]
-    [InlineData(0.5d, 0.875d)]
-    [InlineData(0.75d, 0.984375d)]
-    [InlineData(1d, 1d)]
-    public void EaseOut_WithReferenceSample_ReturnsCubicEaseOut(
+    [MemberData(nameof(ReferenceSamples))]
+    public void Easing_WithReferenceSample_ReturnsExpectedValue(
+        Func<double, double> easing,
         double value,
         double expected)
     {
-        double result = MotionEasing.EaseOut(value);
-
-        result.Should().BeApproximately(expected, 0.000000000001d);
-    }
-
-    [Theory]
-    [InlineData(0d, 0d)]
-    [InlineData(0.25d, 0.44565392967742323d)]
-    [InlineData(0.5d, 0.81900414413722489d)]
-    [InlineData(0.75d, 0.96352085669164911d)]
-    [InlineData(1d, 1d)]
-    public void EaseRail_WithReferenceSample_ReturnsReferenceCubicBezier(
-        double value,
-        double expected)
-    {
-        double result = MotionEasing.EaseRail(value);
-
-        result.Should().BeApproximately(expected, 0.000000000001d);
-    }
-
-    [Theory]
-    [InlineData(0d, 0d)]
-    [InlineData(0.25d, 0.812131184615433d)]
-    [InlineData(0.5d, 0.95683848994775467d)]
-    [InlineData(0.75d, 0.99303416534723565d)]
-    [InlineData(1d, 1d)]
-    public void EaseMaterial_WithReferenceSample_ReturnsReferenceCubicBezier(
-        double value,
-        double expected)
-    {
-        double result = MotionEasing.EaseMaterial(value);
+        double result = easing(value);
 
         result.Should().BeApproximately(expected, 0.000000000001d);
     }
